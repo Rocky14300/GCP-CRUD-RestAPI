a546b13d7ae93f4a6ff54f9911d5e8913965017c
Rahul <rahulmvish7@gmail.com>
2024-10-16 16:59:45 +0530

    Initial commit

---
+++ src/main/java/com/GCP_CRUD_RestAPI/GcpCrudRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.GCP_CRUD_RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpCrudRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpCrudRestApiApplication.class, args);
+	}
+
+}
